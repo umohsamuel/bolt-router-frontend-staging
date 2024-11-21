a220d5cb0a5be65ff7199e00e6b5d477ea265a2c
Umoh Samuel Godwin <umohsg@gmail.com>
2024-11-21 09:06:40 +0100

    setup

---
+++ src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  return (
+    <div className="text-7xl font-bold text-red-500">Hello bolt router</div>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+import containerQueries from "@tailwindcss/container-queries"
+
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
+  },
+  plugins: [containerQueries],
+} satisfies Config;
